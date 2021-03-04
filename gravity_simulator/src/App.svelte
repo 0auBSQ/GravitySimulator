e0e7914348d110d7f15a2a120bb0990793ee65b2
root <root@0AuBSQ.localdomain>
2021-03-04 17:21:15 +0100

    all planetes c bo

--- gravity_simulator/src/App.svelte
+++ gravity_simulator/src/App.svelte
@@ -1,38 +1,65 @@
 <script>
 	import { onMount } from 'svelte';
-	import { createScene } from "./scene";
+	import { createScene, refreshScene } from "./scene";
 
 	let el;
   onMount(() => {
     createScene(el)
+		refresh_model();
   });
 
-	export let name;
+	let planetes = [
+		{ id: `https://solartextures.b-cdn.net/2k_earth_daymap.jpg`, text: `Earth`, radius: 1 },
+		{ id: `https://solartextures.b-cdn.net/2k_mercury.jpg`, text: `Mercury`, radius: 0.38 },
+		{ id: `https://solartextures.b-cdn.net/2k_venus_atmosphere.jpg`, text: `Venus`, radius: 0.95 },
+		{ id: `https://solartextures.b-cdn.net/2k_mars.jpg`, text: `Mars`, radius: 0.53 },
+		{ id: `https://solartextures.b-cdn.net/2k_jupiter.jpg`, text: `Jupiter`, radius: 11.20 },
+		{ id: `https://solartextures.b-cdn.net/2k_saturn.jpg`, text: `Saturn`, radius: 9.45 },
+		{ id: `https://solartextures.b-cdn.net/2k_uranus.jpg`, text: `Uranus`, radius: 4 },
+		{ id: `https://solartextures.b-cdn.net/2k_neptune.jpg`, text: `Neptune`, radius: 3.88 },
+	];
+
+	let selected;
+	let answer = '';
+
+	const refresh_model = () => {
+		refreshScene(selected);
+	}
+
 </script>
-
-
-
 
 <canvas bind:this={el}></canvas>
 
+
+<aside class="left">
+
+	<select bind:value={selected} on:change="{() => refresh_model()}">
+		{#each planetes as planete}
+			<option value={planete}>
+				{planete.text}
+			</option>
+		{/each}
+	</select>
+
+</aside>
+
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.left {
+		position: absolute;
+		top: 0;
+		left: 0;
+		padding: 0;
+		margin: 0;
+		height: 100vh;
+		width: 15vw;
+		background-color: rgba(80, 80, 80, 0.5);
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	canvas {
+		position: absolute;
+		top: 0;
+		left: 0;
+		padding: 0;
+		margin: 0;
 	}
 </style>
--- gravity_simulator/src/scene.js
+++ gravity_simulator/src/scene.js
@@ -3,38 +3,47 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const geometry = new THREE.BoxGeometry();
+
 let material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const cube = new THREE.Mesh(geometry, material);
+let geometry = new THREE.SphereBufferGeometry(1, 64, 64);
+
+//const cube = new THREE.Mesh(geometry, material);
 
 const loader = new THREE.TextureLoader();
 
-let planet = new THREE.Mesh(new THREE.SphereBufferGeometry(1, 32, 32), material);
+let planet = new THREE.Mesh(geometry, material);
 scene.add(planet);
 
-loader.load(
-	// resource URL
-	'https://solartextures.b-cdn.net/2k_earth_daymap.jpg',
-	// onLoad callback
-	function (texture) {
-		// in this example we create the material when the texture is loaded
-		material = new THREE.MeshBasicMaterial({map: texture});
-    planet.material = material;
-    console.log("loaded");
-	},
-	// onProgress callback currently not supported
-	undefined,
-	// onError callback
-	function ( err ) {
-		console.error( 'An error happened.' );
-	}
-);
+const reload_material = (link, ratio) => {
+  loader.load(
+  	// resource URL
+  	link,
+  	// onLoad callback
+  	function (texture) {
+  		// in this example we create the material when the texture is loaded
+  		material = new THREE.MeshBasicMaterial({map: texture});
+      geometry = new THREE.SphereBufferGeometry(ratio, 64, 64);
+      planet.material = material;
+      planet.geometry = geometry;
+      camera.position.z = 2 * ratio;
+      camera.position.y = ratio;
+      console.log("loaded");
+  	},
+  	// onProgress callback currently not supported
+  	undefined,
+  	// onError callback
+  	function ( err ) {
+  		console.error( 'An error happened.' );
+  	}
+  );
+};
 
 
 let renderer;
 //scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 2;
+camera.position.y = 1;
 
 
 const animate = () => {
@@ -54,6 +63,11 @@ export const createScene = (el) => {
   renderer = new THREE.WebGLRenderer({ antialias: true, canvas: el });
   resize();
   animate();
+}
+
+export const refreshScene = (el) => {
+  console.log(el);
+  reload_material(el.id, el.radius);
 }
 
 window.addEventListener('resize', resize);
